de3a4e06cfbaa375fd4421bfc7615378d87d3eb3
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2023-10-02 16:02:59 -0300

    feat: added navbar and home

--- frontend-consurural/src/App.tsx
+++ frontend-consurural/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
+import HomeComponent from './components/HomeComponent/HomeComponent';
+import NavbarHome from './components/NavbarHome/NavbarHome';
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <div className="background-home">
+        <NavbarHome />
+        <HomeComponent />
+      </div>
+    </div>
   )
 }
 
---
+++ frontend-consurural/src/components/HomeComponent/HomeComponent.tsx
@@ -0,0 +1,21 @@
+import "../HomeComponent/HomeComponent.css";
+
+const HomeComponent = () => {
+    return (
+        <div>
+            <div className="brand">
+                <h1 className="consurural-name-brand">
+                    Consurural
+                </h1>
+
+                <hr className="underlined-consurural-brand" />
+                
+                <h2 className="text-background-image">
+                    Prestando consultorias a propriedades rurais a mais de 10 anos
+                </h2>
+            </div>
+        </div>
+    )
+}
+
+export default HomeComponent;
