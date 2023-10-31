867adfe7894538cb28d5bb5581bc0335391b64db
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2023-10-31 14:49:38 -0300

    feat: card recomendations

--- frontend-consurural/src/components/CardRecommendations/CardRecomendations.tsx
+++ frontend-consurural/src/components/CardRecommendations/CardRecomendations.tsx
@@ -1,16 +1,17 @@
+import "./CardRecomendations.css";
+
 const CardRecomendations = () => {
     return (
-        <div>
-            <div className="card-recomendations">
-                <div className="text-recomendations">
-                    <h1>
-                        Quisque posuere, 
-                        purus ut egestas vehicula, 
-                        nisl ante aliquet neque, 
-                        vitae consectetur nisi erat vitae massa.
-                    </h1>
-                </div>
+        <div className="container-recomendations">
+            <h1 className="name-nav-point">RECOMENDAÇÕES</h1>
+            <div className="text-recomendations">
+                Quisque posuere, 
+                purus ut egestas vehicula, 
+                nisl ante aliquet neque, 
+                vitae consectetur nisi erat vitae massa.
             </div>
+
+            <img src="" alt="icon-happy" />
         </div>
     )
 }
