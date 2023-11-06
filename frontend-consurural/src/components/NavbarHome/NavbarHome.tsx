4e718860e9937bd6971b6d4850c53a0461d043db
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2023-11-06 20:48:34 -0300

    deploy

--- frontend-consurural/src/components/NavbarHome/NavbarHome.tsx
+++ frontend-consurural/src/components/NavbarHome/NavbarHome.tsx
@@ -1,6 +1,5 @@
 import "./NavbarHome.css";
 import { Menubar } from "primereact/menubar";
-import { InputText } from "primereact/inputtext";
 
 const NavbarHome = () => {
   const items = [
@@ -22,40 +21,10 @@ const NavbarHome = () => {
     },
   ];
 
-  const start = <div></div>;
-  const end = (
-    <div className="p-menu-list">
-      {items.map((e) => (
-        <button className="">{e.label} | </button>
-      ))}
-    </div>
-  );
-
   interface items {
     label: string;
     icon: string;
   }
-
-  /* function EndMenu({ items }: { items: items[] }) {
-    return (
-      <div className="p-menubar">
-        {items.map((item, index) => (
-          <li key={index}>
-            <a
-              style={{
-                color: "black",
-              }}
-              role="menuitem"
-              className="p-menuitem-link"
-            >
-              {item.label}
-            </a>
-          </li>
-        ))}
-        <button className="button-consulting rounded">CONSULTORIA</button>
-      </div>
-    );
-  } */
 
   return (
     <div className="card">
@@ -66,34 +35,11 @@ const NavbarHome = () => {
           <img
             src="/src/assets/icons/icon.png"
             className="nav-icon w-12 md:w-16"
-            style={
-              {
-                //   height: 100,
-                //   maxWidth: "40px !important",
-              }
-            }
           />
         }
         end={<button className="button-consulting rounded">CONSULTORIA</button>}
       />
     </div>
-
-    // <div>
-    //   <nav className="nav">
-    //     <ul className="navbar-items">
-    //       <li>Inicio</li>
-    //       <li>Orçamentos</li>
-    //       <li>Depoimentos</li>
-    //       <li>Recomendações</li>
-
-    //       <button className="button-consulting rounded">CONSULTORIA</button>
-    //     </ul>
-    //     <img
-    //       src="/src/assets/icons/icon.png"
-    //       className="nav-icon w-12 md:w-16"
-    //     />
-    //   </nav>
-    // </div>
   );
 };
 
